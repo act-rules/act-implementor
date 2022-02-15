0301d6c138170cfab81607f9ef591ccf78e680b3
Wilco Fiers <wilco.fiers@deque.com>
2022-02-15 14:16:09 +0100

    fix: tweak page metadata

--- src/views.ts
+++ src/views.ts
@@ -1,49 +1,56 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from './views/Home.vue';
-import Generate from './views/Generate.vue';
-import GenerateAssertion from './views/GenerateAssertion.vue';
-import About from './views/About.vue';
-import Start from './views/Start.vue';
-import Export from './views/Export.vue';
-import Error from './views/Error.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import Generate from "./views/Generate.vue";
+import GenerateAssertion from "./views/GenerateAssertion.vue";
+import About from "./views/About.vue";
+import Start from "./views/Start.vue";
+import Export from "./views/Export.vue";
+import Error from "./views/Error.vue";
 
 export const router = createRouter({
   history: createWebHashHistory(),
-  routes: [{
-      path: '/',
-      component: Home
-    }, {
-      path: '/about',
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/about",
       component: About,
-      meta: { title: 'About' }
-    }, {
-      path: '/start',
+      meta: { title: "About" },
+    },
+    {
+      path: "/start",
       component: Start,
-      meta: { title: 'Start a report' }
-    }, {
-      path: '/export',
+      meta: { title: "Start a report" },
+    },
+    {
+      path: "/export",
       component: Export,
-      meta: { title: 'Export the report' }
-    }, {
-      path: '/error',
+      meta: { title: "Export the report" },
+    },
+    {
+      path: "/error",
       component: Error,
-      meta: { title: 'An error occurred' }
-    }, {
-      path: '/generate',
+      meta: { title: "An error occurred" },
+    },
+    {
+      path: "/generate",
       component: Generate,
-      meta: { title: 'Implementation overview' }
-    }, {
-      path: '/generate/:ruleId',
+      meta: { title: "Implementation overview" },
+    },
+    {
+      path: "/generate/:ruleId",
       component: GenerateAssertion,
-      meta: { title: 'Update an implementation' }
+      meta: { title: "Update an implementation" },
     },
   ],
-})
+});
 
-const DEFAULT_TITLE = 'ACT Implementor';
+const baseTitle = window.document.title;
 router.afterEach((to) => {
-  window.document.title = DEFAULT_TITLE;
-  if (typeof to.meta?.title === 'string') {
+  window.document.title = baseTitle;
+  if (typeof to.meta?.title === "string") {
     window.document.title += ` - ${to.meta?.title}`;
   }
 });
